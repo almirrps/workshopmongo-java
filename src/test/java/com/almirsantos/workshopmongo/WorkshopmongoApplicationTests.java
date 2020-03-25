44c365ed3195989efe45126ebddf99a45cf4bcd0
Almir Santos <almirrps@hotmail.com>
2020-03-25 15:23:45 -0300

    Projeto importado do Bitbucket

---
+++ src/test/java/com/almirsantos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almirsantos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
